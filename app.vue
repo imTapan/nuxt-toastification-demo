0ac7b182d6f7170aa4557818fa2d7db3f0fa4a6e
tapan-padaliya <tapanpadaliya02@gmail.com>
2024-10-14 18:23:48 +0530

    add init toast

--- app.vue
+++ app.vue
@@ -1,3 +1,9 @@
 <template>
   <div>Hello World</div>
 </template>
+<script setup>
+const { $toast } = useNuxtApp();
+$toast.success("😃 Yay!!!");
+$toast.warning("😬 Oops!!!");
+$toast.error("😟 Ehhhh!!!");
+</script>
